01ca6afed22ef02e1c096454c8180ff4af9e6abb
George Skouroupathis <georskou@cisco.com>
2016-03-25 01:45:22 +0000

    Implement basic client

--- netpython.py
+++ netpython.py
@@ -2,11 +2,67 @@
 
 import sys, socket, argparse, threading, subprocess
 
+def read_and_send():
+    while True:
+        try:
+            bffr = read_from_stdin()
+        except KeyboardInterrupt:
+            sys.exit(1)
+        try:
+            send_to_client(bffr)
+        except KeyboardInterrupt:
+            continue
+        except:
+            sys.exit(0)
+
+def read_from_stdin():
+    try:
+        print
+        print '___________________________ REQUEST ____________________________'
+        bffr = raw_input()
+        print '________________________________________________________________'
+        print
+        return bffr
+    except KeyboardInterrupt:
+        print '[*] Aborted'
+        raise
+
+def send_to_client(bffr):
+
+    try:
+
+        # send data if there is any
+        if len(bffr):
+            client.send(bffr)
+
+        # wait for data back
+        while True:
+
+            response = client.recv(4096)
+            if response and len(response) < 4096:
+                break
+
+        print
+        print '*************************** RESPONSE ***************************'
+        print response
+        print '****************************************************************'
+        print
+        return 'wat'
+
+    except KeyboardInterrupt:
+        print '[*] Transfer Aborted'
+        raise
+
+    except Exception as exc:
+        print '[*] Exiting:', exc
+        raise
+
 if __name__ == '__main__':
-    print 'main'
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--target', nargs=1, metavar='HOST')
-    parser.add_argument('-p', '--port', nargs=1, metavar='PORT')
+    parser.add_argument('-t', '--target', nargs=1, default='127.0.0.1',
+        metavar='HOST')
+    parser.add_argument('-p', '--port', nargs=1, default=1337, type=int,
+        metavar='PORT')
     parser.add_argument('-l', '--listen', action='store_true',
         help='listen on [host]:[port] for incoming connections')
     parser.add_argument('-e', '--execute', nargs=1, metavar='FILE',
@@ -24,4 +80,11 @@ if __name__ == '__main__':
 
     # if running in client mode
     else:
-        pass
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((args.target, args.port))
+
+        read_and_send()
+
+        client.close()
+
+    print args
